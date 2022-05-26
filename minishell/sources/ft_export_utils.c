fdc20e79a6b8a079bfd3307f015cf6371f353e24
Saad Alaoui <95027314+sghajdao@users.noreply.github.com>
2022-05-26 21:12:57 +0100

    Update ft_export_utils.c

--- minishell/sources/ft_export_utils.c
+++ minishell/sources/ft_export_utils.c
@@ -1,5 +1,21 @@
 #include "minishell.h"
 
+int	plus_search(char *key)
+{
+	int	i;
+	int	flag;
+
+	i = 0;
+	flag = 0;
+	while (key[i])
+	{
+		if (key[i] == '+')
+			flag = 1;
+		i++;
+	}
+	return (flag);
+}
+
 int	ft_strind(char *str, int c)
 {
 	int	i;
@@ -18,13 +34,52 @@ void    print_export(t_struct *mini)
 {
     int i;
 
-    i = 0;
-    while (i < mini->env.len)
-    {
-		if (ft_isalnum(mini->env.content[i][0]))
-        	printf("declare -x %s=\"%s\"\n", mini->env.key[i], mini->env.content[i]);
-		else
-			printf("declare -x %s=%s\n", mini->env.key[i], mini->env.content[i]);
-        i++;
-    }
+    if (!mini->tokens[1])
+	{
+		i = 0;
+    	while (i < mini->env.len)
+    	{
+			if (ft_isalnum(mini->env.content[i][0]))
+    	    	printf("declare -x %s=\"%s\"\n", mini->env.key[i], mini->env.content[i]);
+			else
+				printf("declare -x %s=%s\n", mini->env.key[i], mini->env.content[i]);
+    	    i++;
+    	}
+	}
+}
+
+void	exec_verify(t_struct *mini, char **env_aux, int i)
+{
+	if (env_aux[1])
+	verify_if_env_exists(mini, env_aux, i);
+	else if (mini->tokens[i][ft_strlen(mini->tokens[1]) - 1] == '=')
+	{
+		env_aux[1] = ft_strdup("");
+		verify_if_env_exists(mini, env_aux, i);
+	}
+	free(env_aux[0]);
+	free(env_aux[1]);
+	free(env_aux);
+	env_aux = NULL;
+}
+
+void	exec_export_add(t_struct *mini, char *key, char *value, int i)
+{
+	if (if_plus(key, value, mini))
+		return ;
+	if (find_env(mini, key))
+	{
+		free(mini->env.content[mini->env.index]);
+		mini->env.content[mini->env.index] = ft_strdup(value);
+	}
+	else
+		add_env(mini, key, value);
+	if (!ft_strncmp(mini->tokens[i], "PATH", 4))
+	{
+		if (mini->path)
+			free_char_array(mini->path);
+		init_path(mini);
+	}
+	free(key);
+	free(value);
 }
