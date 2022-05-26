39e926c675d0074e8a38c865c03a82177c4f242c
Saad Alaoui <95027314+sghajdao@users.noreply.github.com>
2022-05-26 17:04:11 +0100

    Update init_path.c

--- minishell/sources/init_path.c
+++ minishell/sources/init_path.c
@@ -4,19 +4,27 @@ int	init_path(t_struct *mini)
 {
 	char	*path_aux;
 	int		i;
+	char	**path;
 
 	path_aux = ft_strdup(find_env(mini, "PATH"));
 	if (!path_aux)
 		return (0);
-	mini->path = ft_split(path_aux, ':');
-	if (!mini->path)
+	path = ft_split(path_aux, ':');
+	if (!path)
 		return (0);
 	i = 0;
-	while (mini->path && mini->path[i])
+	while(path[i])
+		i++;
+	mini->path = (char **)malloc(sizeof(char *) * i);
+	if (!mini->path)
+		return (free(path), 0);
+	i = 0;
+	while (path[i] != NULL)
 	{
-		mini->path[i] = ft_strjoin(mini->path[i], "/");
+		mini->path[i] = ft_strjoin(path[i], "/");
 		i++;
 	}
+	free_char_array(path);
 	free(path_aux);
 	return (1);
 }
