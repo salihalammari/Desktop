7f407e90cecff1722a0ecc7ea527ea4b84b3f6b3
Saad Alaoui <95027314+sghajdao@users.noreply.github.com>
2022-05-31 16:13:12 +0100

    Update redir_out.c

--- minishell/sources/redir_out.c
+++ minishell/sources/redir_out.c
@@ -1,9 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redir_out.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sghajdao <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/29 21:05:10 by sghajdao          #+#    #+#             */
+/*   Updated: 2022/05/29 21:05:11 by sghajdao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
+
+char	*expander(t_struct *mini, char *file)
+{
+	int		i;
+	char	*new_file;
+
+	i = 0;
+	if (file[0] == '$')
+	{
+		while (mini->env.key[i])
+		{
+			if (ft_strncmp(mini->env.key[i], &file[1], ft_strlen(mini->env.key[i])) == 0 && (file[ft_strlen(mini->env.key[i]) + 1] == '\0' || file[ft_strlen(mini->env.key[i]) + 1] == ' '))
+			{
+				new_file = ft_strdup(mini->env.content[i]);
+				return (new_file);
+			}
+			i++;
+		}
+	}
+	return (file);
+}
 
 void	redirect_out(t_struct *mini, int j)
 {
 	int		flags;
 	char	*file;
+	char	*copy;
 
 	flags = O_WRONLY | O_CREAT;
 	j = mini->c;
@@ -12,6 +46,14 @@ void	redirect_out(t_struct *mini, int j)
 		if (mini->commands[j] && mini->commands[j][1] == '>')
 		{
 			file = ft_strtrim(&mini->commands[j][2], " ");
+			take_off_quotes(file);
+			if (file[0] == '$')
+			{
+				copy = ft_strdup(file);
+				free(file);
+				file = expander(mini, copy);
+				free(copy);
+			}
 			mini->out_fd = open(file, flags | O_APPEND, 0777);
 			free (file);
 		}
@@ -27,9 +69,19 @@ void	simple_redir_out(t_struct *mini, int j, int flags)
 {
 	char	*assist;
 	char	*file;
+	char	*copy;
 
 	assist = ft_strtrim(&mini->commands[j][1], " ");
 	file = ft_substr(assist, 0, find_char(assist, ' '));
+	take_off_quotes(file);
+	if (file[0] == '$')
+	{
+		copy = ft_strdup(file);
+		free(file);
+		file = expander(mini, copy);
+		free(copy);
+		copy = NULL;
+	}
 	mini->out_fd = open(file, flags | O_TRUNC, 0777);
 	free (assist);
 	free (file);
