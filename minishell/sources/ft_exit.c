87fdb74f7d4a65de31e5e4261e56886b562c9259
Saad Alaoui <95027314+sghajdao@users.noreply.github.com>
2022-06-01 16:00:42 +0100

    Update ft_exit.c

--- minishell/sources/ft_exit.c
+++ minishell/sources/ft_exit.c
@@ -12,10 +12,52 @@
 
 #include "minishell.h"
 
+bool	all_is_digit(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
 void	ft_exit(t_struct *mini)
 {
 	int	ex;
 
+	printf("exit\n");
+	if (mini->tokens[1] && !mini->tokens[2] && all_is_digit(mini->tokens[1])== true)
+	{
+		free_char_array2(mini->commands);
+		if (*mini->env.env)
+		{
+			free_char_array2(mini->env.content);
+			free_char_array2(mini->env.key);
+		}
+		if (mini->path)
+			free_char_array(mini->path);
+		free(mini->home);
+		close(mini->in_fd);
+		close(mini->out_fd);
+		ex = ft_atoi(mini->tokens[1]);
+		free_char_array(mini->tokens);
+		exit(ex);
+	}
+	else if (mini->tokens[2])
+	{
+		printf("minishell: exit: too many arguments\n");
+		return ;
+	}
+	else if (mini->tokens[1] && !mini->tokens[2] && all_is_digit(mini->tokens[1]) == false)
+	{
+		printf("bash: exit: %s: numeric argument required\n", mini->tokens[1]);
+		return ;
+	}
 	free_char_array2(mini->commands);
 	if (*mini->env.env)
 	{
@@ -27,14 +69,6 @@ void	ft_exit(t_struct *mini)
 	free(mini->home);
 	close(mini->in_fd);
 	close(mini->out_fd);
-	//free(mini->line);
-	printf("exit\n");
-	if (mini->tokens[1])
-	{
-		ex = ft_atoi(mini->tokens[1]);
-		free_char_array(mini->tokens);
-		exit(ex);
-	}
 	free_char_array(mini->tokens);
 	exit(0);
 }
