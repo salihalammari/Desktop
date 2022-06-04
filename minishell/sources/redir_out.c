01512e2cb9143f19153c953af3b7e3c75137c49e
Saad Alaoui <95027314+sghajdao@users.noreply.github.com>
2022-06-04 19:00:37 +0100

    Update redir_out.c

--- minishell/sources/redir_out.c
+++ minishell/sources/redir_out.c
@@ -12,27 +12,6 @@
 
 #include "minishell.h"
 
-char	*expander(t_struct *mini, char *file)
-{
-	int		i;
-	char	*new_file;
-
-	i = 0;
-	if (file[0] == '$')
-	{
-		while (mini->env.key[i])
-		{
-			if (ft_strncmp(mini->env.key[i], &file[1], ft_strlen(mini->env.key[i])) == 0 && (file[ft_strlen(mini->env.key[i]) + 1] == '\0' || file[ft_strlen(mini->env.key[i]) + 1] == ' '))
-			{
-				new_file = ft_strdup(mini->env.content[i]);
-				return (new_file);
-			}
-			i++;
-		}
-	}
-	return (file);
-}
-
 void	redirect_out(t_struct *mini, int j)
 {
 	int		flags;
