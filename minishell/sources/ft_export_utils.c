866f16b7dfe7fcdc0e04692d580283048ed7e464
Saad Alaoui <95027314+sghajdao@users.noreply.github.com>
2022-06-05 15:10:04 +0100

    Update ft_export_utils.c

--- minishell/sources/ft_export_utils.c
+++ minishell/sources/ft_export_utils.c
@@ -42,12 +42,101 @@ int	ft_strind(char *str, int c)
 	return (0);
 }
 
+void    sort_2D_str(t_struct *mini, int len)
+{
+    int        i;
+    int        j;
+    char    *tmp;
+
+    i = -1;
+    j = 0;
+    while(++i < len)
+    {
+        j = i;
+        while (++j < len)
+        {
+            if (mini->sorted.env[j] && ft_strncmp(mini->sorted.env[i], mini->sorted.env[j], ft_strlen(mini->sorted.env[i])) > 0)
+            {
+                tmp = ft_strdup(mini->sorted.env[i]);
+                free(mini->sorted.env[i]);
+                mini->sorted.env[i] =ft_strdup(mini->sorted.env[j]);
+                free(mini->sorted.env[j]);
+                mini->sorted.env[j] = ft_strdup(tmp);
+                free(tmp);
+            }
+        }
+    }
+    mini->sorted.env[i] = NULL;
+}
+
 void    print_export(t_struct *mini)
+{
+    int i;
+	char	**env_aux;
+
+	i = 0;
+	mini->sorted.env = malloc(sizeof(char *) * (mini->env.len + 1));
+	while (mini->env.env[i])
+	{
+		mini->sorted.env[i] = ft_strdup(mini->env.env[i]);
+		i++;
+	}
+	i = 0;
+	while (mini->sorted.env[i])
+		i++;
+	mini->sorted.len = i;
+	mini->sorted.key = malloc(sizeof(char *) * (mini->sorted.len + 1));
+	if (!mini->sorted.key)
+		exit(EXIT_FAILURE);
+	mini->sorted.content = malloc(sizeof(char *) * (mini->sorted.len + 1));
+	if (!mini->sorted.content)
+		exit(EXIT_FAILURE);
+	sort_2D_str(mini, mini->sorted.len);
+	i = 0;
+	while (mini->sorted.env[i])
+	{
+		env_aux = ft_split(mini->sorted.env[i], '=');
+		if (!env_aux)
+		{
+			printf("malloc error\n");
+			exit(1);
+		}
+		mini->sorted.key[i] = ft_strdup(env_aux[0]);
+		if (env_aux[1] && ft_strncmp(env_aux[0], "OLDPWD", 6))
+			mini->sorted.content[i] = ft_strdup(env_aux[1]);
+		else if (env_aux[1] && ft_strncmp(env_aux[0], "OLDPWD", 6) == 0)
+			mini->sorted.content[i] = ft_strdup(find_env(mini, "PWD"));
+		else
+			mini->sorted.content[i] = ft_strdup("");
+		free_char_array(env_aux);
+		env_aux = NULL;
+		i++;
+	}
+	mini->sorted.key[i] = NULL;
+	mini->sorted.content[i] = NULL;
+    if (!mini->tokens[1])
+	{
+		i = 0;
+    	while (i < mini->sorted.len)
+    	{
+			if (!(mini->sorted.content[i][0] == '\"' && mini->sorted.content[i][1] == '\"') && mini->sorted.content[i][0] != '\0')
+				printf("declare -x %s=\"%s\"\n", mini->sorted.key[i], mini->sorted.content[i]);
+			else if (mini->sorted.content[i][0] == '\0')
+				printf("declare -x %s\n", mini->sorted.key[i]);
+			else
+				printf("declare -x %s=%s\n", mini->sorted.key[i], mini->sorted.content[i]);
+    	    i++;
+    	}
+	}
+}
+
+/* void    print_export(t_struct *mini)
 {
     int i;
 
     if (!mini->tokens[1])
 	{
+		sort_2D_str(mini, mini->env.len);
 		i = 0;
     	while (i < mini->env.len)
     	{
@@ -60,7 +149,7 @@ void    print_export(t_struct *mini)
     	    i++;
     	}
 	}
-}
+} */
 
 void	exec_verify(t_struct *mini, char **env_aux, int i)
 {
