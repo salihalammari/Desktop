9954a88c858c45d170508465217afca2b02c7b33
Saad Alaoui <95027314+sghajdao@users.noreply.github.com>
2022-06-08 11:17:06 +0100

    Update ft_exit.c

--- minishell/sources/ft_exit.c
+++ minishell/sources/ft_exit.c
@@ -1,3 +1,4 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -47,6 +48,9 @@ void	ft_exit(t_struct *mini)
 		ex = ft_atoi(mini->tokens[1]);
 		free_char_array(mini->tokens);
 		free(mini->line_read);
+		free_char_array(mini->sorted.env);
+		free_char_array(mini->sorted.key);
+		free_char_array(mini->sorted.content);
 		exit(ex);
 	}
 	else if (mini->tokens[1] && mini->tokens[2])
@@ -65,13 +69,17 @@ void	ft_exit(t_struct *mini)
 		free_char_array2(mini->env.content);
 		free_char_array2(mini->env.key);
 	}
-	if (mini->path)
+	if (mini->env_flag == 0)
 		free_char_array(mini->path);
-	free(mini->home);
+	if (there_is_home(mini) == true)
+		free(mini->home);
 	free(mini->line_read);
 	close(mini->in_fd);
 	close(mini->out_fd);
 	free_char_array(mini->tokens);
+	free_char_array(mini->sorted.env);
+	free_char_array(mini->sorted.key);
+	free_char_array(mini->sorted.content);
 	exit(0);
 }
 
